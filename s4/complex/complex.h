41a17bbb950ecff00104cc121408ddcd613a16e6
nowrep <nowrep@gmail.com>
2013-03-10 15:13:28 +0100

    s4: Coding style
    + added *.swp into .gitignore

--- s4/complex/complex.h
+++ s4/complex/complex.h
@@ -3,17 +3,20 @@
 #include <iostream>
 
 class Complex {
-	float re, im;
-public:
-	Complex(float _re = 0, float _im = 0);
-	void print() const;
-	float abs() const;
-	float getRe() const;
-	float getIm() const;
+    float re, im;
 
-	friend Complex operator+(const Complex& a, const Complex& b);
-	friend Complex operator-(const Complex& a, const Complex& b);
-	friend std::ostream& operator<< (std::ostream& os, const Complex& c);
+public:
+    Complex(float _re = 0, float _im = 0);
+
+    void print() const;
+    float abs() const;
+    float getRe() const;
+    float getIm() const;
+
+    friend Complex operator+(const Complex& a, const Complex& b);
+    friend Complex operator-(const Complex& a, const Complex& b);
+    friend std::ostream& operator<< (std::ostream& os, const Complex& c);
 };
 
-#endif
+#endif // __COMPLEX_H_
+
